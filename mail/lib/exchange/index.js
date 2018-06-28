75ee511659de2e8c171490d74e10272ea9e1fc59
mdkardaev <Kardaev.74@gmail.com>
2018-06-28 11:34:11 +0500

    EX-342: fillLimitOrder and marketOrder

--- mail/lib/exchange/index.js
+++ mail/lib/exchange/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   userLoggedIn: require('./lib/userLoggedIn'),
   profileUpdated: require('./lib/profileUpdated'),
-  limitOrderPublished: require('./lib/limitOrderPublished')
+  limitOrderPublished: require('./lib/limitOrderPublished'),
+  limitOrderFilled: require('./lib/limitOrderFilled'),
+  marketOrderExecuted: require('./lib/marketOrderExecuted')
 }
